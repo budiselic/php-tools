70b648ea26fa69069f427cc3216f932abbd7c4e8
Antonio Budiselić <antonio.budiselic@gmail.com>
2017-10-02 11:52:14 +0200

    Update example.php

--- example.php
+++ example.php
@@ -6,7 +6,7 @@ use Tools;
 
 $validator = new Tools\Validator();
 
-if($validator->validateEmail('antonio.budiselic@gmail.com')) {
+if($validator->validateEmail('john.doe@gmail.com')) {
 	echo 'Valid email address!!!';
 } else {
 	echo 'Invalid email address!!!';
